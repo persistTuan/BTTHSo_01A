85b3048b580e2369e8d59eff0917e55682d08658
persistTuan <khanghoa7896@gmail.com>
2023-09-25 10:39:48 +0700

    complete

--- mysql.php
+++ mysql.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
     class Mysql{
         private static $pdo = null;
         // public static ?string $query = null;
